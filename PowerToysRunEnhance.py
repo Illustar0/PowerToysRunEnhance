ea4f73c265ecb190ddc9fd28560c063165887a35
Illustar0 <me@illustar0.com>
2024-11-13 00:17:25 +0800

    feat: add config file

--- PowerToysRunEnhance.py
+++ PowerToysRunEnhance.py
@@ -4,7 +4,12 @@ import os
 import string
 from pynput import keyboard
 import pyautogui
+import toml
 
+
+config = toml.load('config.toml')
+searchWindowName = config['settings']['searchWindowName']
+powerToysRunHotKey = config['settings']['powerToysRunHotKey']
 alphabet = list(string.ascii_lowercase)
 
 
@@ -12,7 +17,7 @@ def on_press(key):
     try:
         if key.char.lower() in alphabet:
             comtypes.CoInitialize()
-            window = automation.WindowControl(Name="搜索", className="Windows.UI.Core.CoreWindow", searchDepth=2)
+            window = automation.WindowControl(Name=searchWindowName, className="Windows.UI.Core.CoreWindow", searchDepth=2)
             if window.Exists(0, 0):
                 RichEditBox = window.EditControl(AutomationId="SearchTextBox", className="RichEditBox")
                 ValuePattern = RichEditBox.GetPattern(automation.PatternId.ValuePattern)
@@ -21,7 +26,7 @@ def on_press(key):
                 else:
                     value = ValuePattern.Value
                 os.kill(window.ProcessId, 9)
-                pyautogui.hotkey("alt", "space")
+                pyautogui.hotkey(*powerToysRunHotKey.split('+'))
                 pyautogui.typewrite(value)
 
             comtypes.CoUninitialize()
